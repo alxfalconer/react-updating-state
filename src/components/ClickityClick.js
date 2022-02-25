822dd64fb8c28596c308d8a447ecd38a52bbb106
Alexander <al.x.falconer@gmail.com>
2022-02-25 14:00:00 +0100

    1st commit

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,22 @@
 // Code ClickityClick Component Here
+import React from "react";
+
+export default class ClickityClick extends React.Component {
+  constructor() {
+    super();
+  }
+
+  handleClick = () => {
+    this.setState({
+      hasBeenClicked: true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click me!</button>
+      </div>
+    );
+  }
+}
